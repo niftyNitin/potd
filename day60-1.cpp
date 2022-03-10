9d6da4d1ced53a7ef7a870f3119fb6986c2c10fa
Nitin Gupta <gupta10nitin99@gmail.com>
2022-03-10 20:53:09 +0530

    Minimum in rotated sorted array

--- day60-1.cpp
+++ day60-1.cpp
@@ -1,3 +1,23 @@
+// Question Link: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+
+// Approach 1 (Linear Search)
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1)  return nums[0];
+        else{
+            bool flag = 0;
+            for(int i = 0; i < n-1; i++){
+                if(nums[i] > nums[i+1])
+                    return nums[i+1];
+            }
+            return nums[0];
+        }
+    }
+};
+
+// Approach 2 (Binary Search)
 class Solution {
 public:
 
